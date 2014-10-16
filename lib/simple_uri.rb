53f1e114683eeef4d124ce7627d7a4b10d7a8ddc
NikSan <nikulinaleksandr@gmail.com>
2014-10-16 11:25:25 +0400

    fix some troubles

--- lib/simple_uri.rb
+++ lib/simple_uri.rb
@@ -13,7 +13,7 @@ module SimpleUri
     #parameters:url-String, method-Symbol||String, params-String, user-String, password:String, debug-Boolean
     def connect(url: nil, method: nil, params: nil, user: nil, password: nil, debug: @@debug_mode)
       enable_debug_mode(debug)
-      uri = URI.parse(URI.encode(url))
+      uri = URI.parse(URI.encode(prepare_url(url)))
       http = Net::HTTP.new(uri.host, uri.port)
       http.read_timeout = 1000
       debug_http(http)
@@ -30,7 +30,7 @@ module SimpleUri
     #parameters:url-String, method-Symbol||String, params-String, req_body-String, req_headers-Hash, user-String, password:String, debug-Boolean, cookies-Boolean
     def send_request(url: nil, method: nil, params: nil, req_body: nil, req_headers: nil, user: nil, password: nil, debug: @@debug_mode, cookies: false)
       req, http = connect(url: url, method: method, params: params, user: user, password: password, debug: debug)
-      req.body = req_body
+      req.body = prepare_req_body(req_body)
       req_headers.each { |k, v| req[k] = v } if req_headers
       res = http.request(req)
       res.body
@@ -43,11 +43,23 @@ module SimpleUri
       cookies ? { body: res_body, cookies: res.response['set-cookie'] } : res_body
     end
 
-    def body_to_str_params(body)
-      body.map { |k, v| "#{k.to_s}=#{v.to_s}" }.join('&')
-    end
-
     private
+      
+      def prepare_req_body(body)
+        (body.is_a?(Hash))? body_to_str_params(body) : body
+      end
+
+      def body_to_str_params(body)
+        body.map { |k, v| "#{k.to_s}=#{v.to_s}" }.join('&')
+      end
+      
+      def prepare_url(url)
+        unless url.match(/http(s)?:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.\w{2,5}\/.+/) && url[-1]=='/'
+          url += '/'
+          debug_msg 'Append \'/\' to url.'
+        end
+        url
+      end
 
       def enable_debug_mode(enabled)
         if enabled
